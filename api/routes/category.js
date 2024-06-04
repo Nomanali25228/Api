ad4f2d3f0499c7bb47447d771d8c9e3947c4efce
Your Name <nomi4698dg@gmail.com>
2024-06-04 07:51:22 -0700

    You

---
+++ api/routes/category.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Category = require('../model/category');
+const cloudinary = require('cloudinary').v2;
+const checkauth = require('../middleware/check-auth')
+
+
+cloudinary.config({
+    cloud_name:'dhqbmpldd',
+    api_key:'736582853365184',
+    api_secret:'h31mWUol9sOkIWLzLN5Ns8sujGY'
+  });
+
+
+
+router.get('/',checkauth,(req,res,next)=>{
+    Category.find()
+    .select(' _id name photo')
+    .then(result=>{
+       res.status(200).json({
+           category:result
+       }) 
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        })
+    })
+})
+
+// save category
+router.post('/' ,(req,res,next)=>{
+    console.log(req);
+    console.log(req.files);
+    const file = req.files.photo;
+    cloudinary.uploader.upload(file.tempFilePath,(err,result)=>{
+      console.log(result);
+      category = new Category({
+        _id:new mongoose.Types.ObjectId,
+        name:req.body.name,
+        photo:result.url
+      });
+      category.save()
+      .then(result=>{
+        console.log(result);
+        res.status(200).json({
+          new_category:result
+        })
+      })
+      .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+          error:err
+        })
+      })
+    });
+  
+  })
+
+  //get single category by id
+router.get('/:id',(req,res,next)=>{
+  const _id = req.params.id;
+  Category.findById(_id)
+  .select('_id name photo')
+  .then(result=>{
+    // console.log(result)
+    res.status(200).json({
+      category:result
+    })
+  })
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json({
+      error:err
+    })
+  })
+})
+
+
+  // update
+router.put('/:id',(req,res,next)=>{
+  console.log(req.params.id);
+  const file = req.files.photo;
+  console.log(file);
+  cloudinary.uploader.upload(file.tempFilePath,(err,result)=>{
+    console.log(result);
+    Category.findOneAndUpdate({_id:req.params.id},{
+      $set:{
+        name:req.body.name,
+        photo:result.url
+      }
+    })
+    .then(result=>{
+      res.status(200).json({
+        updated_category:result
+      })
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(500).json({
+        error:err
+      })
+    })
+  })
+
+})
+
+  
+
+// router.delete('/:id',(req,res,next)=>{
+//     console.log(req.params.id);
+//     Category.findByIdAndRemove(req.params.id)
+//     .then(result=>{
+//         res.status(200).json({
+//             message:'category deleted',
+//             result:result
+//         })
+//     })
+//     .catch(err=>{
+//         console.log(err);
+//         res.status(500).json({
+//             error:err
+//         })
+//     })
+// })
+router.delete('/', (req, res, next) => {
+  const imageUrl = req.query.imageUrl;
+  const urlArray = imageUrl.split('/');
+  console.log(urlArray)
+  const image = urlArray[urlArray.length - 1]
+  console.log(image)
+  const imageName = image.split('.')[0]
+  console.log(imageName)
+  categoryId = req.query.id;
+  Category.remove({ _id: categoryId })
+      .then(result => {
+          cloudinary.uploader.destroy(imageName,(error,result)=>{
+              console.log(error,result);
+          })
+          res.status(200).json({
+              message: result
+          })
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json({
+              error: err
+          })
+      })
+})
+
+
+
+module.exports = router;
