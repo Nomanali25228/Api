811ae4d7054f446e204b9474c8176488b214675e
Your Name <nomi4698dg@gmail.com>
2024-06-05 22:36:02 -0700

    Normalize all the line endings

--- app.js
+++ app.js
@@ -2,42 +2,47 @@ const express = require('express');
 const app = express();
 const productRoute = require('./api/routes/product');
 const userRoute = require('./api/routes/user');
-const categorypath = require('./api/routes/category')
+const categorypath = require('./api/routes/category');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { urlencoded, json } = require('body-parser');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
+// Update the MongoDB connection string
+const mongoURI = 'mongodb+srv://noman:nomi123@cluster0.93xpkzw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect('mongodb+srv://noman:nomi123@cluster0.93xpkzw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{useNewUrlParser:true, useUnifiedTopology: true});
-
-
-mongoose.connection.on('error',err=>{
-  console.log('connection failed');
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-mongoose.connection.on('connected',()=>{
+mongoose.connection.on('error', err => {
+  console.log('connection failed:', err);
+});
+
+mongoose.connection.on('connected', () => {
   console.log('connected successfully with database');
 });
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Configure express-fileupload to use the /tmp directory
 app.use(fileUpload({
-  useTempFiles:true
-}))
+  useTempFiles: true,
+  tempFileDir: '/tmp/',
+}));
 
 app.use(cors());
 
-app.use('/product',productRoute);
-app.use('/user',userRoute);
-app.use('/category',categorypath);
+app.use('/product', productRoute);
+app.use('/user', userRoute);
+app.use('/category', categorypath);
 
-app.get('*',(req,res,next)=>{
+app.use('*', (req, res) => {
   res.status(500).json({
-    message:'bad request'
-  })
-})
+    message: 'bad request',
+  });
+});
 
 module.exports = app;
